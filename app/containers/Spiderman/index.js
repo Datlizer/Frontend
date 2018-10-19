cba6892e01b9c5e4051b0067ff2eb60aed5c6f07
aswinzz <iit2016106@iiita.ac.in>
2018-10-20 02:36:38 +0530

    add line chart

--- app/containers/Spiderman/index.js
+++ app/containers/Spiderman/index.js
@@ -12,7 +12,7 @@ import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 
 
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { LineChart, Line, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
@@ -33,6 +33,12 @@ let carr=[];
 let label=[];
 let data=[];
 let labelData=[];
+
+const spider = [{name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+{name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+{name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+{name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+{name: 'Page E', uv: 1890, pv: 4800, amt: 2181}]
 
 export class Spiderman extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
@@ -124,7 +130,15 @@ export class Spiderman extends React.Component { // eslint-disable-line react/pr
                   :''}
 
                   {this.props.match.params.type=="line"?
-                    ''
+                  	<LineChart width={600} height={300} data={data}
+                          margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+                     <XAxis dataKey="name"/>
+                     <YAxis/>
+                     <CartesianGrid strokeDasharray="3 3"/>
+                     <Tooltip/>
+                     <Legend />
+                     <Line dataKey="uv" stroke="#82ca9d" />
+                    </LineChart>
                     :''}
               </Col>
             </Row>
