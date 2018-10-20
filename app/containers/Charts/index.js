ceee7c76ffb5be40f378ea0050470f54737d2a32
Anupam Dagar <dagaranupam@gmail.com>
2018-10-20 13:09:41 +0530

    fix mergeconflict

--- app/containers/Charts/index.js
+++ app/containers/Charts/index.js
@@ -62,15 +62,8 @@ export class Charts extends React.Component { // eslint-disable-line react/prefe
         <Nav />
         <Layout>
           <Content style={{ margin: '0 16px' }}>
-<<<<<<< Updated upstream
-            <br />
-            <br />
-
-
-=======
             <br/>
             <br/> 
->>>>>>> Stashed changes
             <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
               <Row gutter={0}>
                 <Col span={8}>
@@ -178,11 +171,7 @@ export class Charts extends React.Component { // eslint-disable-line react/prefe
                 </Col>
 
               </Row>
-<<<<<<< Updated upstream
-              <br/><br/>
-=======
               
->>>>>>> Stashed changes
 
             </div>
           </Content>
