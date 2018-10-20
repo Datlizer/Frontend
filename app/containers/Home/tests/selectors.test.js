ecc37bbcdf953feb10dbc5bee2bf34851e59c24a
aswinzz <iit2016106@iiita.ac.in>
2018-10-20 17:33:27 +0530

    remove unwanted components

--- app/containers/Home/tests/selectors.test.js
+++
@@ -1,8 +0,0 @@
-// import { fromJS } from 'immutable';
-// import { selectHomeDomain } from '../selectors';
-
-describe('selectHomeDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-});
