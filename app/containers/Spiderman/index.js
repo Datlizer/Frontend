57691f599c43d5a009bae51836f69a06e8f314f4
aswinzz <iit2016106@iiita.ac.in>
2018-10-21 10:59:58 +0530

    fix color

--- app/containers/Spiderman/index.js
+++ app/containers/Spiderman/index.js
@@ -189,16 +189,7 @@ export class Spiderman extends React.Component { // eslint-disable-line react/pr
                   <Button onClick={()=>this.exportChart(true)} type="primary" shape="circle" icon="download" size='large' />
                 </Popover>
                 <Col span={16}>
-                  <defs>
-                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                      <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-                      <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
-                    </linearGradient>
-                    <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                      <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
-                      <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
-                    </linearGradient>
-                  </defs>
+
                   {this.props.match.params.type == "pie" ?
                     <PieChart ref={(chart) => this.currentChart = chart} id="currentChart" width={800} height={400}>
                       <Pie data={data} cx={200} cy={200} outerRadius={150} fill="#8884d8" />
@@ -213,7 +204,7 @@ export class Spiderman extends React.Component { // eslint-disable-line react/pr
                       <XAxis dataKey="name" />
                       <YAxis />
                       <Tooltip />
-                      <Bar dataKey="uv" fill="url(#colorUv)" />
+                      <Bar dataKey="uv" fill="#8884d8" />
                     </BarChart>
 
                     : ''}
